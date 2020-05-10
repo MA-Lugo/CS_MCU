cd02c40b3295665450dfdd827a1d9ab0fc67a2e1
Mario A. Lugo <mariolg2404@gmail.com>
2020-05-10 00:27:16 -0500

    optimizacion de libreria GPS en uso de memoria---> ROM=13%,RAM=27-33%

--- GPS_Lib.c
+++ GPS_Lib.c
@@ -14,8 +14,8 @@
 
 int1 GPRMC_ok = 0, GPGGA_ok = 0;
 uint8_t char_number = 0, SentenceType = 0, Term;
-char sentence[6], rawTime[11], rawDate[7], rawSpeed[6], rawCourse[6], rawSatellites[3],
-     rawLatitude[13], rawLongitude[13], rawAltitude[7], buffer[12];
+char sentence[6], rawTime[6], rawSatellites[2],
+     rawLatitude[13], rawLongitude[13], buffer[12];
 
 void stringcpy(char *str1, char *str2, int1 dir = 0) {
   uint8_t chr = 0;
@@ -67,9 +67,9 @@ int1 GPSRead() {
       // Latitude N/S
       if((Term == 4) && (SentenceType == _GPRMC_)) {
         if(buffer[0] == 'N')
-          rawLatitude[0] = '0';
+          rawLatitude[0] = 'N';
         else
-          rawLatitude[0] = '-';
+          rawLatitude[0] = 'S';
       }
 
       // Longitude
@@ -79,35 +79,17 @@ int1 GPSRead() {
       // Longitude E/W
       if((Term == 6) && (SentenceType == _GPRMC_)) {
         if(buffer[0] == 'E')
-          rawLongitude[0] = '0';
+          rawLongitude[0] = 'E';
         else
-          rawLongitude[0] = '-';
-      }
-
-      // Speed
-      if((Term == 7) && (SentenceType == _GPRMC_)) {
-        stringcpy(buffer, rawSpeed);
-      }
-
-      // Course
-      if((Term == 8) && (SentenceType == _GPRMC_)) {
-        stringcpy(buffer, rawCourse);
-      }
-
-      // Date
-      if(Term == 9 && SentenceType == _GPRMC_) {
-        stringcpy(buffer, rawDate);
-      }
-
+          rawLongitude[0] = 'W';
+      }    
+      
       // Satellites
       if((Term == 7) && (SentenceType == _GPGGA_)) {
         stringcpy(buffer, rawSatellites);
       }
 
-      // Altitude
-      if((Term == 9) && (SentenceType == _GPGGA_)) {
-        stringcpy(buffer, rawAltitude);
-      }
+
       Term++;
       char_number = 0;
       break;
@@ -120,56 +102,6 @@ int1 GPSRead() {
   return 0;
 }
 
-uint8_t GPSSecond() {
-  return ((rawTime[4] - '0') * 10 + (rawTime[5] - '0'));
-}
-uint8_t GPSMinute() {
-  return ((rawTime[2] - '0') * 10 + (rawTime[3] - '0'));
-}
-uint8_t GPSHour() {
-  return ((rawTime[0] - '0') * 10 + (rawTime[1] - '0'));
-}
 
-uint8_t GPSDay() {
-  return ((rawDate[0] - '0') * 10 + (rawDate[1] - '0'));
-}
-uint8_t GPSMonth() {
-  return ((rawDate[2] - '0') * 10 + (rawDate[3] - '0'));
-}
-uint8_t GPSyear() {
-  return ((rawDate[4] - '0') * 10 + (rawDate[5] - '0'));
-}
-
-float parse_rawDegree(char *term_) {
-  float term_value = atof(term_)/100;
-  int16_t term_dec = term_value;
-  term_value -= term_dec;
-  term_value  = term_value * 5/3 + term_dec;
-  return term_value;
-}
-
-float Latitude() {
-  return parse_rawDegree(rawLatitude);
-}
-
-float Longitude() {
-  return parse_rawDegree(rawLongitude);
-}
-
-float Altitude() {
-  return atof(rawAltitude);
-}
-
-uint8_t Satellites() {
-  return atoi(rawSatellites);
-}
-
-float Speed() {
-  return (atof(rawSpeed) * 1.852);
-}
-
-float Course() {
-  return atof(rawCourse);
-}
 
 
--- main.c
+++ main.c
@@ -13,8 +13,6 @@
 int t = 0,send_period = 2;
 
 
-
-
 #task(rate=1s,max=1us)
    void BLINK_LED();
 #task(rate=1s,max=100ms)
@@ -56,21 +54,42 @@ void GET_DATA(){
 
 }
 
+void CLEAR_DATA(){
+rawTime[0]=0;rawTime[1]=0;rawTime[2]=0;rawTime[3]=0;rawTime[4]=0;rawTime[5]=0;
+
+rawLatitude[0]=0;rawLatitude[1]=0;rawLatitude[2]=0;rawLatitude[3]=0;
+rawLatitude[4]=0;rawLatitude[5]=0;rawLatitude[6]=0;rawLatitude[7]=0;
+rawLatitude[8]=0;rawLatitude[9]=0;rawLatitude[10]=0;rawLatitude[11]=0;
+
+rawLongitude[0]=0;rawLongitude[1]=0;rawLongitude[2]=0;rawLongitude[3]=0;
+rawLongitude[4]=0;rawLongitude[5]=0;rawLongitude[6]=0;rawLongitude[7]=0;
+rawLongitude[8]=0;rawLongitude[9]=0;rawLongitude[10]=0;rawLongitude[11]=0;
+
+rawSatellites[0]=0;rawSatellites[1]=0;
+}
 void SEND_DATA(){ 
    
    t++;
    GET_DATA();
    if(t == send_period){
       fprintf(GS,"#CAN,");
-      fprintf(GS,"%02u,",GPSHour());
-      fprintf(GS,"%02u,",GPSMinute());
-      fprintf(GS,"%02u,",GPSSecond());
-      fprintf(GS,"%.6f,", Latitude());
-      fprintf(GS,"%.6f,", Longitude());
+      fprintf(GS,"%c%c,",rawTime[0],rawTime[1]);
+      fprintf(GS,"%c%c,",rawTime[2],rawTime[3]);
+      fprintf(GS,"%c%c,",rawTime[4],rawTime[5]);
+      fprintf(GS,"%c%c%c",rawLatitude[0],rawLatitude[1],rawLatitude[2]);
+      fprintf(GS,"%c%c%c",rawLatitude[3],rawLatitude[4],rawLatitude[5]);
+      fprintf(GS,"%c%c%c",rawLatitude[6],rawLatitude[7],rawLatitude[8]);
+      fprintf(GS,"%c%c%c,",rawLatitude[9],rawLatitude[10],rawLatitude[11]);
+      fprintf(GS,"%c%c%c",rawLongitude[0],rawLongitude[1],rawLongitude[2]);
+      fprintf(GS,"%c%c%c",rawLongitude[3],rawLongitude[4],rawLongitude[5]);
+      fprintf(GS,"%c%c%c",rawLongitude[6],rawLongitude[7],rawLongitude[8]);
+      fprintf(GS,"%c%c%c,",rawLongitude[9],rawLongitude[10],rawLongitude[11]);
+      fprintf(GS,"%c%c,",rawSatellites[0],rawSatellites[1]);
       fprintf(GS,"\n\r");
-      fprintf(GS,"#SAT");
+      fprintf(GS,"#SAT,");
       fprintf(GS,"\n\r");
       t=0;
+      CLEAR_DATA();
    }
    
 }
